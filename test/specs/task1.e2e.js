bb821d29203fdb5c91fa0adc6864d46762e8ffe8
AlexEnache97 <alex.enache97@yahoo.com>
2021-12-23 16:13:13 +0200

    Test for popular searches and search criteria

--- test/specs/task1.e2e.js
+++ test/specs/task1.e2e.js
@@ -5,7 +5,7 @@ const SearchResultsPage = require('../pageobjects/search.page');
 describe('Search for Dubai Marina properties for sale', () => {
     it('should display only Dubai Marina properties for sale', async () => {
         await HomePage.open()
-        await HomePage.search('Buy', 'Dubai Marina');
+        await HomePage.search('Dubai Marina');
 
         await expect(SearchResultsPage.propertyLocation).toBeExisting();
         await expect(SearchResultsPage.propertyLocation).toHaveTextContaining("Dubai Marina");
